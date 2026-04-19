d0edefd0112c0cf21b0de81675960cb90501cb6e
ViMax <it.vimax@gmail.com>
2026-04-19 20:59:39 +0300

    00:58 added the validations

--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,4 +1,8 @@
- export class CreateTaskDto {
-    title: string;
+import { IsNotEmpty, IsString, Length, MaxLength, MinLength } from "class-validator";
 
- }
+export class CreateTaskDto {
+   @IsString()
+   @IsNotEmpty()
+   @Length(2, 40)
+   title!: string;
+}
--- src/task/dto/update-task.dto.ts
+++ src/task/dto/update-task.dto.ts
@@ -1,4 +1,11 @@
+import { IsNotEmpty, IsString, Length, IsBoolean } from "class-validator";
+
 export class UpdateTaskDto {
-    title: string;
-    isCompleated: boolean;
-} 
+    @IsString({message: "Назва задачі повинно бути строкою"})
+    @IsNotEmpty({message: "Назва задачі не може бути пустим"})
+    @Length(2, 40, {message: "Назва задачі повинна бути від 2 до 40 символів"})
+    title!: string;
+
+    @IsBoolean({message: "Статус повинне бути в булевому виразі"})
+    isCompleated!: boolean;
+}  
