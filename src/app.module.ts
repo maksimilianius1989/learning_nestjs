f1cc469688a676fa4ec24afb26b9932395921ac2
ViMax <it.vimax@gmail.com>
2026-04-20 13:08:57 +0300

    removed the task composent

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppTestService } from './app.testService';
-import { TaskModule } from './task/task.module';
 
 @Module({ 
   controllers: [AppController],
   providers: [AppService, AppTestService],
-  imports: [TaskModule],
 })
 export class AppModule {}
