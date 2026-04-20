4d2e8681d11b0b69899d61cca3d428cb7a4e134c
ViMax <it.vimax@gmail.com>
2026-04-20 17:58:39 +0300

    02:02 connect to the Postgresql successful

---
+++ nodejscourse.pgsql
@@ -0,0 +1,6 @@
+CREATE DATABASE nodejscourse
+  WITH OWNER = 'root'
+ENCODING = 'UTF8'
+LC_COLLATE = 'en_US.utf8'
+LC_CTYPE = 'en_US.utf8'
+TEMPLATE = template0;
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppTestService } from './app.testService';
 import { MovieModule } from './movie/movie.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({ 
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5433,
+    username: 'root',
+    password: '123456',
+    database: 'nodejscourse',
+    autoLoadEntities: true, 
+    synchronize: true 
+  }), MovieModule], 
   controllers: [AppController],
   providers: [AppService, AppTestService],
-  imports: [MovieModule],
 })
 export class AppModule {}
- 
+  
