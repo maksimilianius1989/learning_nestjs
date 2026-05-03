a9bd91066be022f4eb8ec06666d189d58e088c49
ViMax <it.vimax@gmail.com>
2026-05-03 21:21:48 +0300

    4:40 created the custom interceptor

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { requestContext } from './common/request-context';
 import { Logger } from './common/middlewares/logger.middleware';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,9 +15,10 @@ async function bootstrap() {
   });
 
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   app.use(Logger);
-
+ 
   await app.listen(3000);
 }
 bootstrap();
