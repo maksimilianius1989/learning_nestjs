ba068868ed207501e04ba66272d1f6b6335f4335
ViMax <it.vimax@gmail.com>
2026-05-07 22:06:34 +0300

    5:39 added validation for  the register request added hash for the password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterRequest } from './dto/register.dto';
+import { hash } from 'argon2';
 
 @Injectable()
 export class AuthService {
@@ -21,7 +22,7 @@ export class AuthService {
       data: {
         name,
         email,
-        password,
+        password: await hash(password),
       },
     });
 
