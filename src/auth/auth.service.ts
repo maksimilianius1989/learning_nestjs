b25fea3c011f0db367eb23dd9ae9dcf2b53eb056
ViMax <it.vimax@gmail.com>
2026-05-09 20:52:46 +0300

    5:56 added the login method

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,28 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterRequest } from './dto/register.dto';
-import { hash } from 'argon2';
+import { hash, verify } from 'argon2';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { jwtPayload } from './interfaces/jwt.interface';
+import ms from 'ms';
+import { LoginRequest } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prismaService: PrismaService) {}
+  private readonly JWT_SECRET: string;
+  private readonly JWT_ACCESS_TOKEN_TTL: string;
+  private readonly JWT_REFRESH_TOKEN_TTL: string;
+
+  constructor(
+    private readonly prismaService: PrismaService,
+    private readonly configSrervice: ConfigService,
+    private readonly jwtSeervice: JwtService,
+  ) {
+    this.JWT_SECRET = configSrervice.getOrThrow<string>('JWT_SECRET');
+    this.JWT_ACCESS_TOKEN_TTL = configSrervice.getOrThrow<string>('JWT_ACCESS_TOKEN_TTL');
+    this.JWT_REFRESH_TOKEN_TTL = configSrervice.getOrThrow<string>('JWT_REFRESH_TOKEN_TTL');
+  }
 
   async register(dto: RegisterRequest) {
     const { name, email, password } = dto;
@@ -26,6 +43,35 @@ export class AuthService {
       },
     });
 
-    return user;
+    return this.generateTokens(user.id);
+  }
+
+  async login(dto: LoginRequest) {
+    const { email, password } = dto;
+
+    const user = await this.prismaService.user.findUnique({
+      where: { email },
+      select: { id: true, password: true },
+    });
+
+    if (!user) throw new NotFoundException('Користувач не знайдений');
+
+    const isValidPassword = await verify(user.password, password);
+
+    if (!isValidPassword) throw new NotFoundException('Користувач не знайдений');
+
+    return this.generateTokens(user.id);
+  }
+
+  private generateTokens(id: string) {
+    const payload: jwtPayload = { id };
+
+    const accessToken = this.jwtSeervice.sign(payload, { expiresIn: this.JWT_ACCESS_TOKEN_TTL as ms.StringValue });
+    const refreshToken = this.jwtSeervice.sign(payload, { expiresIn: this.JWT_REFRESH_TOKEN_TTL as ms.StringValue });
+
+    return {
+      accessToken,
+      refreshToken,
+    };
   }
 }
